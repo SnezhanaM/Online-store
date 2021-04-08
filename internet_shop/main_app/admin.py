63c075c09508a2e1f4a84ed0a481937b9059f95c
SnezhanaM <snezhanamorozova18@gmail.com>
2021-04-08 19:06:29 +0300

    New changes

--- internet_shop/main_app/admin.py
+++ internet_shop/main_app/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField
-
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from .models import *
+
+from PIL import Image
+
+
+class SmartphoneAdminForm(ModelForm):
+
+    VALID_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Минимальное разрешение изображения: {}x{}'.format(*self.VALID_RESOLUTION)
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.VALID_RESOLUTION
+        if img.width < min_width or img.height < min_height:
+            raise ValidationError('Разрешение загруженного изображения меньше минимального')
+        return image
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
+
+    form = SmartphoneAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
